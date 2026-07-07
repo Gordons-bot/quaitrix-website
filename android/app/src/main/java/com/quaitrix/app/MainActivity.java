a5f9c1c083bbbc24e03fe606332eb3d32e4b229e
Krishverly Gundowry <dev@vmi3421930.contaboserver.net>
2026-07-07 21:08:56 +0200

    capacitor project

---
+++ android/app/src/main/java/com/quaitrix/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.quaitrix.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.quaitrix.app',
+  appName: 'Quaitrix',
+  webDir: 'dist'
+};
+
+export default config;
